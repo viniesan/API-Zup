ae3a302dc76b58d6aad03f63dc87b1f8ebe72d1a
Vinicius Santana <viniciusantanapro@gmail.com>
2021-06-12 01:23:41 -0300

    first commit

---
+++ src/main/java/br/com/api/zupapi/ZupApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.zupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupApiApplication.class, args);
+	}
+
+}
